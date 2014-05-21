0398f2c90d2405edbe47dfb58b2f124b2c2841f6
Kenta Murata <mrkn@mrkn.jp>
2014-05-21 19:24:08 +0900

    Use system ruby to install homebrew

--- script/install.sh
+++ script/install.sh
@@ -7,7 +7,7 @@ ruby_version=2.1.1
 clang --version >/dev/null 2>&1
 
 # Install homebrew
-ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go/install)"
+/usr/bin/ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go/install)"
 
 # Install bootstrap packages
 brew install berkeley-db
