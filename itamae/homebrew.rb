225d155abeb113ac823e53f1bed3d7cc829557cf
Kenta Murata <mrkn@mrkn.jp>
2015-11-08 23:06:12 +0900

    Add homebrew recipe for itamae

---
+++ itamae/homebrew.rb
@@ -0,0 +1,14 @@
+brew_prefix = File.expand_path('~/.brew')
+brew_url = 'https://github.com/Homebrew/homebrew/tarball/master'
+
+directory brew_prefix do
+  mode "755"
+end
+
+execute "Download and extract homebrew into #{brew_prefix}" do
+  command "curl -L #{brew_url} | tar xz --strip 1 -C #{brew_prefix}"
+end
+
+link "/usr/local/bin/brew" do
+  to "#{brew_prefix}/bin/brew"
+end
