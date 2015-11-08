3fa36187b4c29227affd4e8c5756ee25258ad1a9
Kenta Murata <mrkn@mrkn.jp>
2015-11-08 23:29:09 +0900

    Update homebrew recipe

--- itamae/homebrew.rb
+++ itamae/homebrew.rb
@@ -7,4 +7,14 @@ end
 
 execute "Download and extract homebrew into #{brew_prefix}" do
   command "curl -L #{brew_url} | tar xz --strip 1 -C #{brew_prefix}"
+  not_if %[test -x #{brew_prefix}/bin/brew]
+end
+
+execute "brew tap homebrew/bundle" do
+  command "PATH=~/.brew/bin:$PATH brew tap homebrew/bundle"
+  not_if %[brew tap | grep homebrew/bundle]
+end
+
+execute "brew update" do
+  command "PATH=~/.brew/bin:$PATH brew update"
 end
