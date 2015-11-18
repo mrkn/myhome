e308c511f03b9bacccc553e7924c489616d74559
Kenta Murata <mrkn@mrkn.jp>
2015-11-18 22:44:37 +0900

    Bump default python3 to 3.5.0

--- itamae/python.rb
+++ itamae/python.rb
@@ -1,16 +1,16 @@
 python_versions = %w[
   2.7.10
-  3.4.3
+  3.5.0
 ]
 
-python_global_version = '3.4.3'
+python_global_version = '3.5.0'
 
 package 'pyenv'
 package 'homebrew/boneyard/pyenv-pip-rehash'
 
 [*python_versions, python_global_version].uniq.each do |version|
   execute "pyenv install #{version}" do
-    command %[eval "$(pyenv init -)"; pyenv install #{version}]
+    command %[eval "$(pyenv init -)"; pyenv install #{version} && PYENV_VERSION=#{version} pip install --upgrade pip]
     not_if  %[eval "$(pyenv init -)"; pyenv versions | grep #{version}]
   end
 end
