5760f099c654c791d2f1dd3fc0c9033681728c9c
Kenta Murata <mrkn@mrkn.jp>
2016-03-11 02:02:19 +0900

    Install homebrew into /opt/brew

--- itamae/homebrew.rb
+++ itamae/homebrew.rb
@@ -1,8 +1,24 @@
-brew_prefix = File.expand_path('~/.brew')
+require 'etc'
+
+brew_prefix = File.expand_path('/opt/brew')
 brew_url = 'https://github.com/Homebrew/homebrew/tarball/master'
 
-directory brew_prefix do
-  mode "755"
+def get_user_and_group()
+  if (login = Etc.getlogin)
+    pw = Etc.getpwnam(login)
+  else
+    pw = Etc.getpwuid
+  end
+  user_name = pw.name
+  gr = Etc.getgrgid(pw.gid)
+  group_name = gr.name
+  [user_name, group_name]
+end
+
+user_name, group_name = get_user_and_group()
+execute "mkdir #{brew_prefix}" do
+  command "sudo sh -c 'mkdir -p #{brew_prefix}; chown #{user_name}:#{group_name} #{brew_prefix}'"
+  not_if %[test -d #{brew_prefix} -a -w #{brew_prefix}]
 end
 
 execute "Download and extract homebrew into #{brew_prefix}" do
@@ -11,10 +27,10 @@ execute "Download and extract homebrew into #{brew_prefix}" do
 end
 
 execute "brew tap homebrew/bundle" do
-  command "PATH=~/.brew/bin:$PATH brew tap homebrew/bundle"
-  not_if %[brew tap | grep homebrew/bundle]
+  command "env PATH=#{brew_prefix}/bin:$PATH brew tap homebrew/bundle"
+  not_if %[env PATH=#{brew_prefix}/bin:$PATH brew tap | grep homebrew/bundle]
 end
 
 execute "brew update" do
-  command "PATH=~/.brew/bin:$PATH brew update"
+  command "env PATH=#{brew_prefix}/bin:$PATH brew update"
 end
