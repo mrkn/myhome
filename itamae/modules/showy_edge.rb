f208a29d9685517466d593023c2e4d7a3d545012
Kenta Murata <mrkn@mrkn.jp>
2016-03-11 21:41:08 +0900

    Add showy_edge recipe

---
+++ itamae/modules/showy_edge.rb
@@ -0,0 +1,49 @@
+require 'fileutils'
+require 'pathname'
+require 'tmpdir'
+
+app_name = 'ShowyEdge'
+version = '3.0.0'
+sha256 = '4a6b16970cb77a9b3e77031e74cf8907a09106ffff1e430eec9c41790579008c'
+url = "https://pqrs.org/osx/ShowyEdge/files/ShowyEdge-#{version}.dmg"
+
+app_path = Pathname("/Applications/#{app_name}.app")
+info_plist = app_path.join("Contents/Info.plist")
+
+if File.file?(info_plist)
+  existing_version = `defaults read #{info_plist} CFBundleVersion`
+end
+
+need_to_install = existing_version.nil? || (version > existing_version)
+
+tmpdir = Dir.mktmpdir
+at_exit do
+  FileUtils.remove_entry_secure(tmpdir)
+end
+
+mountpoint = Pathname(tmpdir).join('vol')
+mountpoint.mkpath
+
+execute "Download #{app_name}" do
+  command "curl -L -O #{url}"
+  cwd tmpdir
+  only_if "#{need_to_install}"
+end
+
+execute "Uninstall existing #{app_name}" do
+  command "rm -rf #{app_path}"
+  only_if "#{need_to_install} && test -d #{app_path}"
+end
+
+execute "Install #{app_name}" do
+  command <<-CMD
+    hdiutil attach #{File.basename(url)} -readonly -mountpoint #{mountpoint} &&
+      cp -r #{mountpoint.join(app_path.basename)} #{app_path.parent};
+    exitcode=$?;
+    hdiutil detach #{mountpoint} || :;
+    exit $exitcode
+  CMD
+
+  cwd tmpdir
+  only_if "#{need_to_install} && test -f #{File.basename(url)}"
+end
