ae00e02d1b2e8e4f51971d934a14483658a3c886
Kenta Murata <mrkn@mrkn.jp>
2016-04-07 14:39:48 +0900

    Modify ruby recipe to improve build options

--- itamae/modules/ruby.rb
+++ itamae/modules/ruby.rb
@@ -22,11 +22,34 @@ package 'ruby-build' do
   options '--HEAD'
 end
 
+brew_prefix = `brew --prefix`.chomp
+gdbm_dir = `brew --prefix gdbm`.chomp
+gmp_dir = `brew --prefix gmp`.chomp
+libyaml_dir = `brew --prefix libyaml`.chomp
+openssl_dir = `brew --prefix openssl`.chomp
+qdbm_dir = `brew --prefix qdbm`.chomp
+readline_dir = `brew --prefix readline`.chomp
+
+configure_opts = [
+  %w[
+    --enable-shared
+    --disable-install-doc
+    --with-out-ext=tk,tk/*
+    --with-dbm-type=qdbm
+  ],
+  "--with-opt-dir=#{brew_prefix}",
+  "--with-gmp-dir=#{gmp_dir}",
+  "--with-libyaml-dir=#{libyaml_dir}",
+  "--with-openssl-dir=#{openssl_dir}",
+  "--with-dbm-dir=#{qdbm_dir}",
+  "--with-readline-dir=#{readline_dir}",
+].join(' ')
+
 [*ruby_versions, ruby_global_version].uniq.each do |version|
   execute "rbenv install #{version}" do
     command <<-CMD
       eval "$(rbenv init -)";
-      rbenv install #{version} &&
+      CONFIGURE_OPTS='#{configure_opts}' rbenv install #{version} &&
         RBENV_VERSION='#{version}' gem update --system &&
         RBENV_VERSION='#{version}' gem install bundler itamae
     CMD
