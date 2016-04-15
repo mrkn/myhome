0a53b30ab3a231fb7a66748c68bccfb71385924b
Kenta Murata <mrkn@mrkn.jp>
2016-04-15 14:51:40 +0900

    Fix iTerm 2 recipe

--- itamae/modules/iterm2.rb
+++ itamae/modules/iterm2.rb
@@ -2,15 +2,17 @@ require 'fileutils'
 require 'pathname'
 require 'tmpdir'
 
+app_name = "iTerm"
 version = '2.1.4'
 sha256 = '1062b83e7808dc1e13362f4a83ef770e1c24ea4ae090d1346b49f6196e9064cd'
 url = "https://iterm2.com/downloads/stable/iTerm2-#{version.gsub('.', '_')}.zip"
+archive_name = File.basename(url)
 
-app_path = Pathname("/Applications/iTerm.app")
+app_path = Pathname("/Applications/#{app_name}.app")
 info_plist = app_path.join("Contents/Info.plist")
 
 if File.file?(info_plist)
-  existing_version = `defaults read #{info_plist} CFBundleVersion`
+  existing_version = `defaults read '#{info_plist}' CFBundleVersion`
 end
 
 need_to_install = existing_version.nil? || (version > existing_version)
@@ -21,18 +23,18 @@ at_exit do
 end
 
 execute "Download iTerm 2" do
-  command "curl -L -O #{url}"
+  command "curl -L -O '#{url}'"
   cwd tmpdir
   only_if "#{need_to_install}"
 end
 
-execute "Uninstall existing iTerm 2" do
-  command "rm -rf /Applications/iTerm.app"
-  only_if "#{need_to_install} && test -d /Applications/iTerm.app"
+execute "Uninstall existing #{app_name}" do
+  command "rm -rf '#{app_path}'"
+  only_if "#{need_to_install} && test -d '#{app_path}'"
 end
 
-execute "Install iTerm 2" do
-  command "unzip -x -d /Applications #{File.basename(url)}"
+execute "Install #{app_name}" do
+  command "unzip -x -d /Applications '#{archive_name}'"
   cwd tmpdir
-  only_if "#{need_to_install} && test -f #{File.basename(url)}"
+  only_if "#{need_to_install} && test -f '#{archive_name}'"
 end
