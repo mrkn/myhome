b04437de378da7d659d777b04c05149cd95ebf2f
Kenta Murata <mrkn@mrkn.jp>
2016-05-06 18:08:36 +0900

    Install neovim python module

--- itamae/modules/neovim.rb
+++ itamae/modules/neovim.rb
@@ -1,3 +1,9 @@
 package 'neovim/neovim/neovim' do
   options '--env=std'
 end
+
+include_recipe 'python'
+
+execute 'pip install neovim' do
+  command %[eval "$(pyenv init -)"; pip install neovim]
+end
