1543cb29af0963a6634d09120ba9fa30cda1ae08
Kenta Murata <mrkn@mrkn.jp>
2016-05-22 20:55:29 +0900

    Add tensorflow_deps module

---
+++ itamae/modules/tensorflow_deps.rb
@@ -0,0 +1,19 @@
+include_recipe './bazel.rb'
+include_recipe './swig.rb'
+include_recipe './python.rb'
+
+execute "pip install -U six" do
+  command %[eval "$(pyenv init -)"; pyenv exec pip install -U six]
+end
+
+execute "pip install -U numpy" do
+  command %[eval "$(pyenv init -)"; pyenv exec pip install -U numpy]
+end
+
+execute "pip install -U wheel" do
+  command %[eval "$(pyenv init -)"; pyenv exec pip install -U numpy]
+end
+
+execute "pip install jupyter" do
+  command %[eval "$(pyenv init -)"; pyenv exec pip install -U numpy]
+end
