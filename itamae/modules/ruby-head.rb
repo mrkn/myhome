9bc3a07c2d09bef89221e2f1311cdd57391bad57
Kenta Murata <mrkn@mrkn.jp>
2016-06-17 10:41:46 +0900

    Separate ruby-head module

--- itamae/modules/ruby-head.rb
+++ itamae/modules/ruby-head.rb
@@ -1,3 +1,5 @@
+version = '2.4.0-dev'
+
 include_recipe 'gdbm'
 include_recipe 'git'
 include_recipe 'gmp'
@@ -6,14 +8,6 @@ include_recipe 'openssl'
 include_recipe 'qdbm'
 include_recipe 'readline'
 include_recipe 'rbenv'
-
-ruby_versions = %w[
-  2.1.10
-  2.2.5
-  2.3.1
-]
-
-ruby_global_version = '2.3.1'
 
 brew_prefix = `brew --prefix`.chomp
 gdbm_dir = `brew --prefix gdbm`.chomp
@@ -38,23 +32,24 @@ configure_opts = [
   "--with-readline-dir=#{readline_dir}",
 ].join(' ')
 
-[*ruby_versions, ruby_global_version].uniq.each do |version|
-  execute "rbenv install #{version}" do
-    command <<-CMD
-      eval "$(rbenv init -)";
-      CONFIGURE_OPTS='#{configure_opts}' rbenv install #{version} &&
-        RBENV_VERSION='#{version}' gem update --system &&
-        RBENV_VERSION='#{version}' gem install bundler itamae
-    CMD
+optflags = "-O0 -mtune=native -march=native"
+debugflags = "-g3 -gdwarf-4"
+verbose = '-v' if Itamae.logger.level == ::Logger::DEBUG
 
-    not_if <<-CMD unless version.end_with?('-dev')
-      eval "$(rbenv init -)";
-      rbenv versions | grep '#{version}'
-    CMD
-  end
-end
+execute "rbenv uninstall -f #{version} || :"
 
-execute "rbenv global #{ruby_global_version}" do
-  command %[eval "$(rbenv init -)"; rbenv global '#{ruby_global_version}']
-  not_if  %[eval "$(rbenv init -)"; rbenv versions | grep '* #{ruby_global_version}']
+execute "rm -rf ~/.rbenv/sources/#{version} || :"
+
+execute "rbenv install #{version}" do
+  command <<-CMD
+    eval "$(rbenv init -)";
+    CONFIGURE_OPTS='#{configure_opts}' optflags='#{optflags}' debugflags='#{debugflags}' RUBY_CFLAGS='${optflags} ${debugflags} ${warnflags}' rbenv install -k #{verbose} #{version} &&
+      RBENV_VERSION='#{version}' gem update --system &&
+      RBENV_VERSION='#{version}' gem install bundler itamae
+  CMD
+
+  not_if <<-CMD unless version.end_with?('-dev')
+    eval "$(rbenv init -)";
+    rbenv versions | grep '#{version}'
+  CMD
 end
