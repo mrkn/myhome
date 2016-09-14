4ab72a3c5d75ae878f4a79d8e3885344170c0451
Kenta Murata <mrkn@mrkn.jp>
2016-09-14 09:29:19 +0900

    Do not use docker-machine-driver-xhyve

--- itamae/modules/docker.rb
+++ itamae/modules/docker.rb
@@ -1,9 +1,3 @@
 package 'docker'
-
 package 'docker-machine'
-
-package 'docker-machine-driver-xhyve'
-execute 'sudo chown root:wheel $(brew --prefix)/opt/docker-machine-driver-xhyve/bin/docker-machine-driver-xhyve'
-execute 'sudo chmod u+s $(brew --prefix)/opt/docker-machine-driver-xhyve/bin/docker-machine-driver-xhyve'
-
 package 'docker-compose'
